0535f7556c1742f5508a48608e822e0409164689
”Alexis <bonnet.alexis06@gmail.com>
2022-06-22 11:31:04 +0200

    Requete Procedure

--- client/src/components/Procedure.js
+++ client/src/components/Procedure.js
@@ -1,69 +1,31 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import File from '../image/procedure.svg';
-
-const ProceduresTab = [
-    {
-        id:1,
-        name:'Procédure 1',
-        categorie:1
-    },
-    {
-        id:2,
-        name:'Procédure 2',
-        categorie:1
-    },
-    {
-        id:3,
-        name:'Procédure 3',
-        categorie:2
-    },
-    {
-        id:4,
-        name:'Procédure 4',
-        categorie:2
-    },
-    {
-        id:5,
-        name:'Procédure 5',
-        categorie:3
-    },
-    {
-        id:6,
-        name:'Procédure 6',
-        categorie:3
-    },
-    {
-        id:7,
-        name:'Procédure 7',
-        categorie:4
-    },
-    {
-        id:8,
-        name:'Procédure 8',
-        categorie:4
-    },
-    {
-        id:9,
-        name:'Procédure 9',
-        categorie:5
-    },
-    {
-        id:10,
-        name:'Procédure 10',
-        categorie:5
-    }
-]
+import axios from 'axios';
 
 const Procedure = () => {
+    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoidGVzdCBsb2dpbiAyIiwiaWF0IjoxNjU1ODI1NzI0LCJleHAiOjE2NTU5MTIxMjR9.mkTE9cdGQg572Q_ML6XZA-SZjvUb64FcUN3dH-tODJA';
+
+    const [procedures,setProcedures] = useState();
+    useEffect(() => {
+        const getProcedures = async () =>{
+            const response = await axios
+            .post('http://localhost:8000/articleReq/getAllArticle.php',
+            {key:'value'},
+            {headers:{Authorization: `Bearer ${token}`}})
+            setProcedures(response.data)
+        }
+        getProcedures();
+    },[])
+
     return (
         <div className='Procedure'>
-            {ProceduresTab.map( ProcedureTab => 
+            {procedures?.map( procedure => 
                 <div className='Card'>
                     <div className='CardImage'>
                         <img src={File} />
                     </div>
                     <div className='CardTitle'>
-                        <p key={ProcedureTab.id}>{ProcedureTab.name}</p>
+                        <p key={procedure.id}>{procedure.nom}</p>
                     </div>
                 </div>
             )}
