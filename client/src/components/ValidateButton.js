49657c54ceec4721c21e2b782dcc1bf5dd99f0b4
”Alexis <bonnet.alexis06@gmail.com>
2022-06-23 14:02:45 +0200

    Valide

--- client/src/components/ValidateButton.js
+++ client/src/components/ValidateButton.js
@@ -3,10 +3,8 @@ import Valider from '../image/check.svg';
 
 const ValidateButton = () => {
     return (
-        <div>
-            <div className='ValidateButton'>
-                <img src={Valider}></img>
-            </div>
+        <div className='ValidateButton'>
+            <img src={Valider}></img>
         </div>
     );
 };
